f6abe367203b3f20c2dc92dbf01910833e7a4eea
Michael Winter <mlwinter@gmail.com>
2015-10-26 18:18:41 +0000

    Addressed an issue where media introduced by an active form would not be rendered in templates.
    bdr.templates:
     # Added the media for forms in the category addition, deletion, and editing templates.
     # Added pagination support to the category detail template.
     # Made a minor presentation change to the pagination widget in the category list template.
    
    bdr.views:
     # Added pagination to the CategoryDetailView class in the categories module.

--- bdr/views/categories.py
+++ bdr/views/categories.py
@@ -3,7 +3,8 @@ This module defines classes for displaying and editing categories.
 """
 
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import DetailView, ListView, UpdateView, DeleteView, CreateView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from django.views.generic.detail import SingleObjectMixin
 
 from . import SearchableViewMixin
 from ..forms import CategoryForm
@@ -30,11 +31,61 @@ __license__ = """
     """
 
 
-class CategoryDetailView(SearchableViewMixin, DetailView):
+class CategoryDetailView(SearchableViewMixin, SingleObjectMixin, ListView):
     """Displays a detailed view of a given category."""
 
     model = Category
+    paginate_by = 10
+    paginate_orphans = 2
     template_name = "bdr/categories/category_detail.html"
+
+    def __init__(self, **kwargs):
+        super(CategoryDetailView, self).__init__(**kwargs)
+        self.object = None
+
+    def get(self, request, *args, **kwargs):
+        """
+        Send a response for HTTP GET requests sent to this view.
+
+        :param request: The request received by the view.
+        :type request: django.http.HttpRequest
+        :param args: The positional arguments captured from the route for this
+                     view.
+        :param kwargs: The named arguments captured from the route for this
+                       view.
+        :return: The response.
+        :rtype: django.http.HttpResponseBase
+        """
+        self.object = self.get_object(queryset=self.model.objects.all())
+        return super(CategoryDetailView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        Return the template context for this view.
+
+        This method returns a dictionary containing variables for the rendered
+        view. Available template context variables are:
+
+         * ``category`` - this category
+         * ``datasets`` - the datasets associated with this category
+
+        :param kwargs: A mapping of extra data available for use in templates.
+        :type kwargs: dict of str
+        :return: A dictionary of template variables and values.
+        :rtype: dict of str
+        """
+        context = super(CategoryDetailView, self).get_context_data(**kwargs)
+        context["datasets"] = context["object_list"]
+        return context
+
+    def get_queryset(self):
+        """
+        Get the list of datasets associated with this category.
+
+        :return: The datasets associated with this category.
+        :rtype: ``django.db.models.query.QuerySet`` | list
+        """
+        return self.object.datasets.all()
 
 
 class CategoryListView(SearchableViewMixin, ListView):
